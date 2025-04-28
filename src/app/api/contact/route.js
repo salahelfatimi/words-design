10d2da6115f7c603153022a35c35c2755df9c617
salahelfatimi <salahfatimi16@gmail.com>
2025-04-28 11:22:38 +0100

    add

--- src/app/api/contact/route.js
+++ src/app/api/contact/route.js
@@ -7,20 +7,16 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export async function POST(request) {
   try {
     const body = await request.json();
-    const { serviceUrl, fullName, email, tele, comment, checkView ,type, adresse} = body;
+    const { fullName, email, tele, comment} = body;
     const data = await resend.emails.send({
       from: `${fullName}<onboarding@dayafandco.com>`,
-      to: "dayafandco@gmail.com",
-      subject: serviceUrl ? "`Dayaf & Co - Demande de participation à une activité ou d'information sur l'immobilier`" :type?("Le client a contacté via la page de Vendre.") :("Le client a contacté via la page de contact."),
+      to: "salahfatimi16@gmail.com",
+      subject: 'Le client a contacté via la page de contact . ',
       react: EmailTemplate({
-        serviceUrl: serviceUrl,
         fullName: fullName,
         comment: comment,
         email: email,
         Phone: tele,
-        checkView:checkView,
-        typeVendre:type,
-        adresseVendre:adresse
       }),
     });
 
